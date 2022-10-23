3e5ef36d860b075f23f4124372bf4d792d79dcb2
calebtuttle <1calebtuttle@gmail.com>
2022-10-23 16:48:27 -0400

    Add getStateAsHexString

---
+++ index.js
@@ -0,0 +1,9 @@
+import { stateAbbreviations } from "./constants";
+
+export function getStateAsHexString(state, countryCode) {
+  // TODO: Support all countries and states/provinces
+  if (!state || countryCode != 2) return "0x";
+  state = state.length == 2 ? state : stateAbbreviations[state.toUpperCase()];
+  const uint8Array = new TextEncoder("utf-8").encode(state);
+  return "0x" + uint8Array.map((x) => x.toString(16).padStart(2, "0")).join("");
+}
